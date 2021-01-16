107fe2a92373f23e35e4f358a6bf9a16cf0b8ba5
Maneesh <maneeshsriram123@gmail.com>
2021-01-17 00:17:14 +0530

    Update views.py

--- text/text/views.py
+++ text/text/views.py
@@ -9,16 +9,45 @@ def home(request):
 def analyze(request):
     txt = request.GET.get('text', 'default')
     is_punc = request.GET.get('remPunc', 'off')
+    is_fullCaps = request.GET.get('fullCaps', 'off')
+    is_extraspace = request.GET.get('remSpace', 'off')
+    is_remLine = request.GET.get('remLine', 'off')
 
     if is_punc == 'on':
-        punc = '''!()-[]{};:'"\, <>./?@#$%^&*_~'''
+        punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
         for i in txt:
-            if i in punc:
-                txt = txt.replace(i, "")
-        analyzed = txt
+            if i not in punc:
+                analyzed = analyzed + i
 
         params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
         return render(request, 'analyze.html', params)
 
+    elif is_fullCaps == 'on':
+        analyzed = ""
+        for i in txt:
+            analyzed = analyzed + i.upper()
+
+        params = {'purpose': 'Capitalized letters', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+
+    elif is_extraspace == "on":
+        analyzed = ""
+        for index, char in enumerate(txt):
+            if not(txt[index] == " " and txt[index+1] == " "):
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Removed Extra spaces', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+
+    elif is_remLine == 'on':
+        analyzed = ""
+        for i in txt:
+            if i != '\n' and i != "\r":
+                analyzed = analyzed + i
+
+        params = {'purpose': 'Removed blank lines', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+
     else:
-        return HttpResponse('Remove punctuations not ticked')
+        return HttpResponse('<h1>Please tick any checkbox</h1>')
